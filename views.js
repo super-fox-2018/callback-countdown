c204a19893b6062390989865ac0d2ba2137ff830
adrowicaksono <adrowicaksono@gmail.com>
2018-05-10 23:45:56 +0700

    callback countdown

--- views.js
+++ views.js
@@ -6,6 +6,26 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let minutes = Math.floor(seconds / 60);
+    let sec = Math.floor(seconds % 60);
+    let result = '';
+    if(minutes < 10){
+      result += "0" + minutes;
+    }
+    else{
+      result += minutes;
+    }
+  
+    if(sec < 10){
+      result += ":0" + sec;
+    }
+    else{
+      result += ":" + sec;
+    }
+    console.log(chalk.cyan(figlet.textSync(result)));
+
 };
 
 module.exports = { displayCountdown };
+
+
