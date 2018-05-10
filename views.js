75ac78c9b59e11fd8fe1a7bdde295b1307353d79
handipriyono <handimacbook@gmail.com>
2018-05-10 19:19:54 +0700

    add views.js

--- views.js
+++ views.js
@@ -4,8 +4,19 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan.bold(figlet.textSync("Countdown App")));
+  let menit = Math.floor(seconds / 60)
+  let second = seconds - (menit * 60)
+  if (String(second).length === 1) {
+    second = '0' + second
+  }
+  if (String(menit).length === 1) {
+    menit = '0' + menit
+  }
+
+  console.log(chalk.white.bold(figlet.textSync(menit + " : " + second)))
 };
 
-module.exports = { displayCountdown };
+module.exports = {
+  displayCountdown
+};
