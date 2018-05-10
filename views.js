4bb64e3ccbf3a28d1732788389c5d937ecc0923b
justinusamwijaya <justin.am.wijaya@gmail.com>
2018-05-10 19:37:22 +0700

    countdown finished

--- views.js
+++ views.js
@@ -7,7 +7,7 @@ const displayCountdown = seconds => {
   console.log(chalk.cyan(figlet.textSync("My Timer Countdown")));
   // Your code here...
   let minutes = parseInt(seconds/60)
-  let secs = seconds - minutes >= 10 && seconds%60 !== 0? ` : ${seconds - minutes*60}` : ` : 0${seconds - minutes*60}`
+  let secs = seconds - minutes*60 >= 10 && seconds%60 !== 0? ` : ${seconds - minutes*60}` : ` : 0${seconds - minutes*60}`
   minutes = minutes >= 10? `${minutes}` : `0${minutes}`
   if(seconds === 'FINISHED!'){
     minutes = seconds
