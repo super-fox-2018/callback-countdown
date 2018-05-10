b2b75c4e7ad87481cd3cec54a8d706323958e244
justinusamwijaya <justin.am.wijaya@gmail.com>
2018-05-10 19:00:00 +0700

    countdown finished

--- views.js
+++ views.js
@@ -4,8 +4,16 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync("My Timer Countdown")));
   // Your code here...
+  let minutes = parseInt(seconds/60)
+  let secs = seconds - minutes >= 10 && seconds%60 !== 0? ` : ${seconds - minutes*60}` : ` : 0${seconds - minutes*60}`
+  minutes = minutes >= 10? `${minutes}` : `0${minutes}`
+  if(seconds === 'FINISHED!'){
+    minutes = seconds
+    secs = ''
+  }
+  console.log(chalk.cyan(figlet.textSync(`${minutes}${secs}`)));
 };
 
 module.exports = { displayCountdown };
