09b5b89536e1247d4c655259a76121be6722a002
Rhesa Utomo <venderblitz@gmail.com>
2018-05-10 22:47:35 +0700

    release 1

--- views.js
+++ views.js
@@ -6,7 +6,16 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Timer")));
   // Your code here...
-  console.log(secondFormatter(seconds));
+  let secondStr = secondFormatter(seconds)
+
+  if (seconds <= 5) {
+    console.log(chalk.red(figlet.textSync(secondStr)));
+  } else if (seconds < 10 && seconds > 5) {
+    console.log(chalk.yellow(figlet.textSync(secondStr)));
+  } else {
+    console.log(chalk.green(figlet.textSync(secondStr)));
+
+  }
 
 };
 
