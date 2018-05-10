def7c71c520196fa752cad3aa237f35cec2cb08f
Dwiky Erlangga <dwikyerl@gmail.com>
2018-05-10 17:02:12 +0700

    Finished Callback Countdown Challenge

--- controllers.js
+++ controllers.js
@@ -1,7 +1,13 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let time = seconds;
+  displayCountdown(time);
+  const intervalId = setInterval(() => {
+    time -= 1;
+    displayCountdown(time);
+    if(time === 0) clearInterval(intervalId);
+  }, 1000);
 };
 
 module.exports = {
