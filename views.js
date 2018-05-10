fff91a64285fac06baa08f67bcb06ad960d764d1
Rhesa Utomo <venderblitz@gmail.com>
2018-05-10 22:24:17 +0700

    release 0

--- views.js
+++ views.js
@@ -4,8 +4,19 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync("Timer")));
   // Your code here...
+  console.log(secondFormatter(seconds));
+
 };
+
+function secondFormatter(seconds) {
+  let minute = Math.floor(seconds / 60);
+  if (seconds >= 60) { seconds = seconds - (minute * 60) }
+  if (seconds < 10) { seconds = `0${seconds}` }
+  if (minute < 10) { minute = `0${minute}` }
+
+  return `${minute}:${seconds}`
+}
 
 module.exports = { displayCountdown };
