076b896c31a122a8d78c1dc1ff515006cf2e24fd
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-05-12 21:57:26 +0700

    brama-prasetyo

--- controllers.js
+++ controllers.js
@@ -1,8 +1,29 @@
-const { displayCountdown } = require("./views");
-
+const {
+  displayCountdown
+} = require("./views");
 const timer = seconds => {
-  // Your code here...
-};
+
+  var countdown = setInterval(function() {
+    if (seconds >= 0) {
+      var setTime = Math.floor(seconds / 60)
+      var timemodulus = Math.floor(seconds % 60)
+      if (setTime <= 9 ) {
+        var showTime = `0${setTime} : 0${timemodulus}`
+        displayCountdown(showTime)
+        seconds--
+      }
+      else if(setTime > 9){
+        var showTime = `${setTime} : ${timemodulus}`
+        displayCountdown(showTime)
+        seconds--
+      }
+
+    } else {
+      clearInterval(countdown)
+    }
+  }, 1000)
+
+}
 
 module.exports = {
   timer
